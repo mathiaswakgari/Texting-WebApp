eeaaec6fbf112ceff0556da60354ad9963dff9fe
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-08-23 14:36:55 +0300

    Added protected route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Grid, GridItem } from "@chakra-ui/react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ReactNode, useContext } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
 import { AuthContext } from "./context/AuthContext";
 
+type Props = {
+  children: ReactNode;
+};
+
 function App() {
-  const currentuser = useContext(AuthContext);
+  const currentUser = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }: Props) => {
+    if (!currentUser) return <Navigate to={"/login"} />;
+  };
 
   return (
     <BrowserRouter>
@@ -22,7 +30,14 @@ function App() {
       >
         <GridItem area="main" w={"100vw"}>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
           </Routes>
