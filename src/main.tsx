ece05353b3b78b9db9f1b95a1f0291ce2ac1d355
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-08-30 10:13:58 +0300

    Changed font.

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App.tsx";
 import "./index.css";
-import { app } from "./services/firebase.ts";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { ChatProvider } from "./context/ChatContext.tsx";
 
 const theme = extendTheme({
   fonts: {
-    body: "Ubuntu",
-    heading: "Ubuntu",
+    body: "Poppins",
+    heading: "Poppins",
   },
 });
 
