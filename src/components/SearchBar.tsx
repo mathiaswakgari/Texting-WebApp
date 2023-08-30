d5702806c22dfd51799e77de482305cbb323d88f
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-08-30 15:16:15 +0300

    Changed style of searchBar.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,31 @@
-import { Box, Input } from "@chakra-ui/react";
-
+import { Box, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { BsSearch } from "react-icons/bs";
 interface Props {
   onChange: (label: string) => void;
 }
 
 const SearchBar = ({ onChange }: Props) => {
   return (
-    <Box w={"full"}>
-      <Input
-        border={"none"}
-        borderRadius={0}
-        borderBottom={"1px solid white"}
-        type="text"
-        placeholder="Search here..."
-        onChange={(e) => onChange(e.currentTarget.value)}
-      />
+    <Box
+      px={1}
+      w={"96"}
+      bg={"whiteAlpha.200"}
+      borderRadius={"3xl"}
+      marginTop={2}
+    >
+      <InputGroup>
+        <InputRightElement marginTop={"5px"} marginRight={3}>
+          <BsSearch color="white" fontSize="20px" />
+        </InputRightElement>
+        <Input
+          border={"none"}
+          type="text"
+          height={"50px"}
+          borderRadius={"3xl"}
+          placeholder="Search here..."
+          onChange={(e) => onChange(e.currentTarget.value)}
+        />
+      </InputGroup>
     </Box>
   );
 };
